82d9001a3e84e34be405745dba0b3fce8280dae5
Marton Biro <marton.biro@mimacom.com>
2017-10-13 08:38:51 +0200

    add security

--- microservice-security/scripts/keycloak/init.sh
+++ microservice-security/scripts/keycloak/init.sh
@@ -7,12 +7,12 @@ export TKN=$(curl -X POST 'http://localhost:8888/auth/realms/master/protocol/ope
  -d 'grant_type=password' \
  -d 'client_id=admin-cli' | jq -r '.access_token')
 
-curl -vX POST --data '@./test-users.json' 'http://localhost:8888/auth/admin/realms/microservice-security-realm/users' \
+curl -vX POST --data '@./microservice-security-realm.json' 'http://localhost:8888/auth/admin/realms' \
 -H "Accept: application/json" \
 -H 'Content-Type: application/json;charset=UTF-8' \
 -H "Authorization: Bearer $TKN" | jq .
 
-curl -vX POST --data '@./microservice-security-realm.json' 'http://localhost:8888/auth/admin/realms' \
+curl -vX POST --data '@./test-users.json' 'http://localhost:8888/auth/admin/realms/microservice-security-realm/users' \
 -H "Accept: application/json" \
 -H 'Content-Type: application/json;charset=UTF-8' \
 -H "Authorization: Bearer $TKN" | jq .
