41fc84f562b84f80c22b46709b4fe905361bef53
Marton Biro <marton.biro@mimacom.com>
2017-10-15 20:20:29 +0200

    Fix keycloak init scripts

--- microservice-security/readme.md
+++ microservice-security/readme.md
@@ -1,12 +1,11 @@
-#Microservice security (not compolete yet)
+# Microservice security (not complete yet)
 
 Example app for experimenting keycloak and oauth2.
 
-##Setup and run the app
+## Setup and run the app
 
 - start keycloak with docker-compose.yml file
 - create a test realm, client and user with the scripts/keycloak/init.sh
-- initialize the password for the test user on the keycloak UI
 - start the car-service spring boot app
 - execute ng build in the webapp directory
 - start the  gateway spring boot app
--- microservice-security/scripts/keycloak/init.sh
+++ microservice-security/scripts/keycloak/init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo '.getting access token'
+
 export TKN=$(curl -X POST 'http://localhost:8888/auth/realms/master/protocol/openid-connect/token' \
  -H "Content-Type: application/x-www-form-urlencoded" \
  -d "username=admin" \
@@ -7,12 +9,24 @@ export TKN=$(curl -X POST 'http://localhost:8888/auth/realms/master/protocol/ope
  -d 'grant_type=password' \
  -d 'client_id=admin-cli' | jq -r '.access_token')
 
-curl -vX POST --data '@./microservice-security-realm.json' 'http://localhost:8888/auth/admin/realms' \
--H "Accept: application/json" \
--H 'Content-Type: application/json;charset=UTF-8' \
--H "Authorization: Bearer $TKN" | jq .
+echo '..creating realm'
 
-curl -vX POST --data '@./test-users.json' 'http://localhost:8888/auth/admin/realms/microservice-security-realm/users' \
+curl -X POST --data '@./microservice-security-realm.json' 'http://localhost:8888/auth/admin/realms' \
 -H "Accept: application/json" \
 -H 'Content-Type: application/json;charset=UTF-8' \
--H "Authorization: Bearer $TKN" | jq .
+-H "Authorization: Bearer $TKN"
+
+echo '...creating test user'
+
+LOCATION=$(curl -iX POST --data '@./test-users.json' 'http://localhost:8888/auth/admin/realms/microservice-security-realm/users' \
+-H "Accept: application/json" \
+-H 'Content-Type: application/json;charset=UTF-8' \
+-H "Authorization: Bearer $TKN" \
+2>&1 | tr -d '\r' | sed -En 's/^Location: (.*)/\1/p')
+
+echo '...setting user1 password'
+
+curl -X PUT --data '@./reset-user1.json' "${LOCATION}/reset-password" \
+-H "Accept: application/json" \
+-H 'Content-Type: application/json;charset=UTF-8' \
+-H "Authorization: Bearer $TKN"
